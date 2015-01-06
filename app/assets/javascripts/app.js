642f7a61320ec31797bd52b58380ebb27636c248
Iris Martinez <i.martinez8@umiami.edu>
2015-01-06 10:06:34 -0500

    starting off the day

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -23,38 +23,36 @@ $(function(){
 			url: "http://gdata.youtube.com/feeds/api/videos?q="+ genre +"&max-results=10&v=2&&key=AIzaSyAvkL7_sQDpLa1g86QL7K4yaEkiV_OGBKc"
 		}).done(function(data){
 			console.log(data)
-			nameTag = data.children[0].children[16].children[12].children[0]
-			console.log(nameTag)
-			authorName = $(nameTag).text()
-			console.log(authorName)
-			published = data.children[0].children[16].children[1].firstChild["data"];
-			updated = data.children[0].children[16].children[2].firstChild["data"];
-			console.log(published)
-			console.log(updated)
-			videoTitle =data.children[0].children[16].children[6].innerHTML
-			console.log(videoTitle)
-			videoUrl = $(data.children[0].children[16].children[7]).attr('src')
-			console.log(videoUrl)
-		}).fail(function(){
-			console.log("failed")
-		}).done(function(){
-			$.ajax({
-				type: "GET",
-				url: "/youtube",	
-				data: {
-					nameTag: nameTag,
-					authorName: authorName,
-					published: published,
-					updated: updated,
-					videoTitle: videoTitle,
-					videoUrl: videoUrl
-				}
-			}).done(function(){
-				console.log("here")
-			}).fail(function(){
-				console.log("boooo")
+			for (i = 16; i< 25; i++){
+				nameTag = data.children[0].children[i].children[14].children[0]
+				console.log(nameTag)
+				authorName = $(nameTag).text()
+				console.log(authorName)
+				published = data.children[0].children[i].children[1].firstChild["data"];
+				updated = data.children[0].children[i].children[2].firstChild["data"];
+				console.log(published)
+				console.log(updated)
+				videoTitle =data.children[0].children[i].children[5].innerHTML
+				console.log(videoTitle)
+				videoUrl = $(data.children[0].children[i].children[6]).attr('src')
+				console.log(videoUrl)
+			}
+				$.ajax({
+					type: "GET",
+					url: "/youtube",	
+					data: {
+						nameTag: nameTag,
+						authorName: authorName,
+						published: published,
+						updated: updated,
+						videoTitle: videoTitle,
+						videoUrl: videoUrl
+					}
+				}).done(function(){
+					console.log("here")
+				}).fail(function(){
+				console.log("failed")
+				})
 			})
 		})
-	})
-})
 
--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -2,23 +2,6 @@ class YoutubeController < ApplicationController
 
 	def show
 		binding.pry
-		genre = params["genre"]
-		posterAge = params["posterAge"]
-		posterGender = params["posterGender"]
-		subscribers= params["subscribers"]
-		monthlyViews = params["monthlyViews"]
-		audienceAge = params["audienceAge"]
-		audienceLocation = params["audienceLocation"],
-		posterLocation = params["posterLocation"]
-
-		url = "http://gdata.youtube.com/feeds/api/videos?q=#{genre}&max-results=10&v=2&key=AIzaSyAvkL7_sQDpLa1g86QL7K4yaEkiV_OGBKc"
-
-		# &dimensions=age#{posterAge},#{posterGender},#{posterLocation}&prettyprint=true&metrics=#{subscribers}
-
-		url.gsub!(/\s+/, "")
-		@data = []
-		data = HTTParty.get(url)
-		@data.push(data)
 
 		render :json=> @data
 	end
